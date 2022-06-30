5faa16f9d2b6f5c2eec05033b77bcdf3c6c00bcc
navintc <navin.thamindu@gmail.com>
2022-06-30 23:58:24 +0530

    Setting up waypoint system

---
+++ Assets/Scripts/WaypointMover.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMover : MonoBehaviour
+{
+
+    // stores reference to waypint system
+    [SerializeField] private Waypoints waypoints;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    [SerializeField] private float distanceThreshold = 1f;
+
+    private Transform currentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set initial position
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        //set next waypoin
+        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+
+        transform.LookAt(currentWaypoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold){
+            currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private void OnDrawGizmos(){
+        foreach (Transform t in transform)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position, 0.4f);
+        }
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i< transform.childCount-1; i++){
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i+1).position);
+        }
+    }
+
+    public Transform GetNextWayPoint(Transform currentWaypoint){
+        if (currentWaypoint == null){
+            return transform.GetChild(0);
+        }
+
+        if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1){
+            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+        } else {
+            return transform.GetChild(0);
+        }
+        return null;
+    }
+}
